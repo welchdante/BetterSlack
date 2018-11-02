5c8fda123ded4ce97c0fe68d649e2c345436c05a
Dante Welch <welchdan@mail.gvsu.edu>
2018-11-02 10:44:45 -0400

    refactored to make client and server classes

--- client/client.py
+++ client/client.py
@@ -1,28 +1,34 @@
-
-# Python program to implement client side of chat room. 
 import socket 
 import select 
 import sys 
-  
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+class Client:
+    def __init__(self, ip, port):  
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        self.server.connect((ip, port))
+
+    # listen for both user input to send out and data from server 
+    def listen(self):
+        while True: 
+            input_streams = [sys.stdin, self.server] 
+            read_sockets, write_socket, error_socket = select.select(input_streams,[],[]) 
+          
+            for socks in read_sockets: 
+                if socks == self.server: 
+                    message = socks.recv(2048) 
+                    print(message.decode()) 
+                else: 
+                    message = sys.stdin.readline() 
+                    self.server.send(message.encode()) 
+                    sys.stdout.write("<You>") 
+                    sys.stdout.write(message) 
+                    sys.stdout.flush() 
+        server.close() 
+
 if len(sys.argv) != 3: 
     exit() 
-IP_address = str(sys.argv[1]) 
-Port = int(sys.argv[2]) 
-server.connect((IP_address, Port)) 
-  
-while True: 
-    input_streams = [sys.stdin, server] 
-    read_sockets,write_socket, error_socket = select.select(input_streams,[],[]) 
-  
-    for socks in read_sockets: 
-        if socks == server: 
-            message = socks.recv(2048) 
-            print(message.decode()) 
-        else: 
-            message = sys.stdin.readline() 
-            server.send(message.encode()) 
-            sys.stdout.write("<You>") 
-            sys.stdout.write(message) 
-            sys.stdout.flush() 
-server.close() 
+ip = str(sys.argv[1]) 
+port = int(sys.argv[2])
+
+client = Client(ip, port)
+client.listen()
